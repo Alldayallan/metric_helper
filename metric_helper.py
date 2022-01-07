526ea12d3a74eb78de0793c5f2351ed6c779002c
Allan <mail@allanreid.co>
2022-01-06 18:11:55 -0800

    useful metric converter

--- metric_helper.py
+++ metric_helper.py
@@ -1,5 +1,5 @@
 import tkinter
-from tkinter import StringVar, font, ttk
+from tkinter import font, ttk, END
 
 #Define window
 root = tkinter.Tk()
@@ -14,6 +14,41 @@ button_color = "#f5cf87"
 root.config(bg=bg_color)
 
 #Define functions
+def convert():
+    """Convert from one metric prefix to another"""
+    metric_values = {
+        'femto':10**-15,
+        'pico':10**-12,
+        'nano':10**-9,
+        'micro':10**-6,
+        'milli':10**-3,
+        'centi':10**-2,
+        'deci':10**-1,
+        'base value':10**0,
+        'deca':10**1,
+        'hecto':10**2,
+        'kilo':10**6,
+        'mega':10**6,
+        'giga':10**9,
+        'tera':10**12,
+        'peta':10**15
+    }
+
+    #Clear output field
+    output_field.delete(0, END)
+
+    #Get all user information
+    start_value = float(input_field.get())
+    start_prefix = input_combobox.get()
+    end_prefix = output_combobox.get()
+
+    #convert to base unit first
+    base_value = start_value*metric_values[start_prefix]
+    #Convert to new metric value
+    end_value = base_value/metric_values[end_prefix]
+
+    #Update output field with answer
+    output_field.insert(0, str(end_value))
 
 #Define layout
 #Create the input and output entry fields
@@ -28,7 +63,7 @@ output_field.grid(row=0, column=2, padx=10, pady=10)
 input_field.insert(0, 'Enter your quantity')
 
 #Create combobox for metric values
-metric_list = ["femto", "pico", "nano", 'micro', 'milli', 'centi', 'deci', 'base value', 'deca', 'hecto', 'kilo', 'mega', 'giga', 'tera', 'peta']
+metric_list = ['femto', 'pico', 'nano', 'micro', 'milli', 'centi', 'deci', 'base value', 'deca', 'hecto', 'kilo', 'mega', 'giga', 'tera', 'peta']
 input_combobox = ttk.Combobox(root, value=metric_list, font=field_font, justify='center')
 output_combobox = ttk.Combobox(root, values=metric_list, font=field_font, justify='center')
 to_label = tkinter.Label(root, text="to", font=field_font, bg=bg_color)
@@ -41,7 +76,7 @@ input_combobox.set('base_value')
 output_combobox.set('base_value')
 
 #Create a conversion button
-convert_button = tkinter.Button(root, text="Convert", font=field_font, bg=button_color)
+convert_button = tkinter.Button(root, text="Convert", font=field_font, bg=button_color, command=convert)
 convert_button.grid(row=2, column=0, columnspan=3, padx=10, pady=10, ipadx=50)
 
 #Run the root window's main loop
